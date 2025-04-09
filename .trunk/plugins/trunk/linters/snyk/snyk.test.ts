41f47c48770d82f99e9a08b8f09a8e57c288e923
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2025-04-09 16:43:08 -0400

    symlinking trunk

--- .trunk/plugins/trunk/linters/snyk/snyk.test.ts
+++
@@ -1,5 +0,0 @@
-import { customLinterCheckTest } from "tests";
-import { TEST_DATA } from "tests/utils";
-
-// Requires SNYK_TOKEN to run
-customLinterCheckTest({ linterName: "snyk", args: TEST_DATA, testName: "basic" });
