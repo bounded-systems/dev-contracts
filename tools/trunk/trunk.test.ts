69cbb83f87e920fcb88c53d161bb5c86117c6757
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2025-04-12 13:00:47 -0400

    new sub modules

--- tools/trunk/trunk.test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
-
-Deno.test("Placeholder test for trunk.ts", () => {
-  // TODO: Implement actual test logic
-  assertEquals(true, true);
-});
